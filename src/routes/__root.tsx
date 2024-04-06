f86d8c26657df920decb93c9b3ededcab2b7f22a
Ajiwojuolorun Joshua <Ajiwojuolorun@relianceinfosystems.com>
2024-04-06 18:48:40 +0100

    Error Fixes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { createRootRoute, Link, Outlet, ScrollRestoration } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+// import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
     component: () => (
