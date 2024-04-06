641ad21429855ebd5d696f6f658378ad55a600f3
Ajiwojuolorun Joshua <Ajiwojuolorun@relianceinfosystems.com>
2024-04-06 18:35:29 +0100

    hot fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-import App from './App.tsx'
 import './index.css'
 
 import { store } from 'app/store'
